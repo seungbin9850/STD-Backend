a7a6b5f9a1bc979d8dde0e2265b14de6d2c65945
senugbin9850 <tanja1206@naver.com>
2021-04-13 09:53:51 +0900

    [ADD] app filter validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import connectionOptions from 'ormconfig';
-import { UserModule } from './user/user.module';
+import { CatchException } from './exception';
+import { UserModule } from './modules';
 
 @Module({
   imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
+  providers: [{ provide: APP_FILTER, useClass: CatchException }],
 })
 export class AppModule {}
