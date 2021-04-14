3e866da5b94d076cdbac0e3541b94cacde0d4daa
senugbin9850 <tanja1206@naver.com>
2021-04-14 14:46:25 +0900

    [ADD] tag entity

---
+++ src/entities/tag.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Post } from './post';
+
+@Entity()
+export class Tag {
+  @PrimaryColumn({ length: 45 })
+  id: string;
+
+  @Column({ length: 20 })
+  tag: string;
+
+  @Column({ name: 'post_id', length: 45 })
+  postId: string;
+
+  @ManyToOne((type) => Post, (post) => post.id, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'post_id' })
+  post: Post;
+}
