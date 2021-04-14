9a003a7c100539e2a44681cd4e2c76cdfd2e6df4
senugbin9850 <tanja1206@naver.com>
2021-04-14 14:45:57 +0900

    [ADD] import post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import connectionOptions from 'ormconfig';
 import { CatchException } from './exception';
-import { UserModule, AuthModule } from './modules';
+import { UserModule, AuthModule, PostModule } from './modules';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions),
+    UserModule,
+    AuthModule,
+    PostModule,
+  ],
   providers: [{ provide: APP_FILTER, useClass: CatchException }],
 })
 export class AppModule {}
