34f59c7362dfc6a57d7d7efdb0a8cd652a47eb88
senugbin9850 <tanja1206@naver.com>
2021-04-14 10:57:11 +0900

    [ADD] verify token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -4,3 +4,7 @@ import config from '../config';
 export const makeToken = async (id: string): Promise<string> => {
   return await sign({ id }, config.jwt, { expiresIn: '2w' });
 };
+
+export const verifyToken = async (token: string): Promise<any> => {
+  return await verify(token, config.jwt);
+};
