8da172c0e9662b0b1a402b8f7782d73000d2d4ae
senugbin9850 <tanja1206@naver.com>
2021-04-15 14:12:39 +0900

    [ADD] deny apply controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/middlewares';
 import { Token } from 'src/utils';
-import { ApplyPostDTO, WritePostDTO } from './dto';
+import { ApplyPostDTO, DenyApplyDTO, WritePostDTO } from './dto';
 import { PostService } from './post.service';
 
 @Controller('posts')
@@ -19,6 +19,13 @@ export class PostController {
   @UseGuards(new AuthGuard())
   async applyPost(@Token() decoded: any, @Body() req: ApplyPostDTO) {
     await this.postService.applyPost(req, decoded);
+    return { status: 200, message: 'success' };
+  }
+
+  @Delete('/apply/deny')
+  @UseGuards(new AuthGuard())
+  async denyApply(@Token() decoded: any, @Body() req: DenyApplyDTO) {
+    await this.postService.denyApply(req, decoded);
     return { status: 200, message: 'success' };
   }
 }
