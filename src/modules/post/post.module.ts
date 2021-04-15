ac05cbb0e8c4191bbfd6cb23db95ac615362ff7f
senugbin9850 <tanja1206@naver.com>
2021-04-15 09:16:45 +0900

    [ADD] import apply repository

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostRepository, TagRepository } from 'src/repositories';
+import {
+  ApplyRepository,
+  PostRepository,
+  TagRepository,
+} from 'src/repositories';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository, TagRepository])],
+  imports: [
+    TypeOrmModule.forFeature([PostRepository, TagRepository, ApplyRepository]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
