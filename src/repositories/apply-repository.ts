4fbfb4c82af13fc2271a42335630d46273db45cc
senugbin9850 <tanja1206@naver.com>
2021-04-15 14:13:04 +0900

    [ADD] delete apply repository

--- src/repositories/apply-repository.ts
+++ src/repositories/apply-repository.ts
@@ -2,4 +2,13 @@ import { Apply } from 'src/entities';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Apply)
-export class ApplyRepository extends Repository<Apply> {}
+export class ApplyRepository extends Repository<Apply> {
+  public async deleteApply(data: { userId: string; postId: string }) {
+    await this.createQueryBuilder('apply')
+      .delete()
+      .from(Apply)
+      .where('post_id = :postId', { postId: data.postId })
+      .andWhere('user_id = :userId', { userId: data.userId })
+      .execute();
+  }
+}
