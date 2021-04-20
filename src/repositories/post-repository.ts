fe40e97725564c4346d168ec5f0505b1071639ec
senugbin9850 <tanja1206@naver.com>
2021-04-20 18:43:16 +0900

    [ADD] delete post repository

--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -10,4 +10,12 @@ export class PostRepository extends Repository<Post> {
       .take(3)
       .getMany();
   }
+
+  public async deletePost(postId: string) {
+    await this.createQueryBuilder('post')
+      .delete()
+      .from(Post)
+      .where('id := postId', { postId })
+      .execute();
+  }
 }
