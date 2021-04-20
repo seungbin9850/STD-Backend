ea6d4611b45b782d2e91cb44f7648890125c52a6
senugbin9850 <tanja1206@naver.com>
2021-04-20 18:42:46 +0900

    [ADD] get all apply repository

--- src/repositories/apply-repository.ts
+++ src/repositories/apply-repository.ts
@@ -11,4 +11,10 @@ export class ApplyRepository extends Repository<Apply> {
       .andWhere('user_id = :userId', { userId: data.userId })
       .execute();
   }
+
+  public async getAllApply(postId: string) {
+    return await this.createQueryBuilder('apply')
+      .where('post_id := postId', { postId })
+      .getMany();
+  }
 }
