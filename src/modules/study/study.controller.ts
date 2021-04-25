d08ec7bffe22a826d132a5ba32d43643856be183
senugbin9850 <tanja1206@naver.com>
2021-04-25 22:13:18 +0900

    [ADD] get studies controller

--- src/modules/study/study.controller.ts
+++ src/modules/study/study.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/middlewares';
 import { Token } from 'src/utils';
 import { CreateStudyDTO } from './dto';
@@ -13,5 +13,12 @@ export class StudyController {
   async createStudy(@Token() decoded: any, @Body() req: CreateStudyDTO) {
     await this.studyService.createStudy(req, decoded.id);
     return { status: 200, message: 'success' };
+  }
+
+  @Get('/')
+  @UseGuards(new AuthGuard())
+  async getMyStudy(@Token() decoded: any) {
+    const data = await this.studyService.getMyStudy(decoded.id);
+    return { status: 200, message: 'success', data };
   }
 }
