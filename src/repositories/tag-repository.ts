39364196131b5f0a63079e4c6347492eaa341a98
senugbin9850 <tanja1206@naver.com>
2021-05-15 01:03:42 +0900

    [FIX] find all tag

--- src/repositories/tag-repository.ts
+++ src/repositories/tag-repository.ts
@@ -1,5 +1,12 @@
 import { Tag } from 'src/entities';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, Repository } from 'typeorm';
 
 @EntityRepository(Tag)
-export class TagRepository extends Repository<Tag> {}
+export class TagRepository extends Repository<Tag> {
+  public async findAllTag(tag: string) {
+    return await getRepository(Tag)
+      .createQueryBuilder('tag')
+      .where('tag.tag = :tag', { tag })
+      .getMany();
+  }
+}
